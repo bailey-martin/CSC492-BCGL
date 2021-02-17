563e1b58696287a57d91908e61431e34214114f5
joshodavidson <52176722+joshodavidson@users.noreply.github.com>
2021-02-16 21:18:59 -0500

    Add project files.

---
+++ BCGL/BCGL/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using BCGL.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace BCGL.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
