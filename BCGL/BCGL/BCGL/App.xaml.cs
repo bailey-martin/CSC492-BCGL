39ebc107321d9f99b1b89a8037a83d4a61a8605d
Andrew <Baitgod01@gmail.com>
2021-03-01 16:20:12 -0500

    Initial DB created
    Still need to customize

--- BCGL/BCGL/BCGL/App.xaml.cs
+++ BCGL/BCGL/BCGL/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using BCGL.Services;
 using BCGL.Views;
 using System;
+using System.IO;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -8,6 +9,19 @@ namespace BCGL
 {
     public partial class App : Application
     {
+        static Database database;
+
+        public static Database Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new Database(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "people.db3"));
+                }
+                return database;
+            }
+        }
 
         public App()
         {
