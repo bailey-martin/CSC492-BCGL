e6b251c0308c24514ed5c290c94820efeb6b36bd
bailey-martin <44448861+bailey-martin@users.noreply.github.com>
2021-03-02 21:56:39 -0500

    Added Link to RAID Inc. Webpage and Added RAID Inc, Logo

--- BCGL/BCGL/BCGL/ViewModels/AboutViewModel.cs
+++ BCGL/BCGL/BCGL/ViewModels/AboutViewModel.cs
@@ -10,7 +10,7 @@ namespace BCGL.ViewModels
         public AboutViewModel()
         {
             Title = "About";
-            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
+            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://bailey-martin.github.io/CSC492-BCGL/"));
         }
 
         public ICommand OpenWebCommand { get; }
