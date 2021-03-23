5ae203817d83cabd10cd4974d01584b5cf8bf1c2
Andrew <Baitgod01@gmail.com>
2021-03-23 17:35:03 -0400

    changed landing page

--- BCGL/BCGL/BCGL/ViewModels/LoginViewModel.cs
+++ BCGL/BCGL/BCGL/ViewModels/LoginViewModel.cs
@@ -29,7 +29,7 @@ namespace BCGL.ViewModels
         private async void OnLoginClicked(object obj)
         {
             // Prefixing with `//` switches to a different navigation stack instead of pushing to the active one
-            await Shell.Current.GoToAsync($"//{nameof(AboutPage)}");
+            await Shell.Current.GoToAsync($"//{nameof(ItemsPage)}");
         }
 
         private async void OnCreateClicked(object obj)
