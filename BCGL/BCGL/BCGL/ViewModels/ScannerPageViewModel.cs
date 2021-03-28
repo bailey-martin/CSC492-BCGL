85f9d87ff7dda62b1905b5f7be2a1a0b0b9ce3ef
bailey-martin <44448861+bailey-martin@users.noreply.github.com>
2021-03-28 15:01:52 -0400

    Live Camera and Saved Photos Working

---
+++ BCGL/BCGL/BCGL/ViewModels/ScannerPageViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BCGL.ViewModels
+{
+    class ScannerPageViewModel
+    {
+
+    }
+}
---
+++ BCGL/BCGL/BCGL/Views/ScannerPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BCGL.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ScannerPage : ContentPage
+    {
+        public ScannerPage()
+        {
+            InitializeComponent();
+        }
+
+        async void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            var result = await MediaPicker.PickPhotoAsync(new MediaPickerOptions
+            {
+                Title = "Please pick a photo"
+            });
+
+            if (result != null)
+            {
+                var stream = await result.OpenReadAsync();
+
+                resultImage.Source = ImageSource.FromStream(() => stream);
+            }
+        }
+
+        async void Button1_Clicked(System.Object sender, System.EventArgs e)
+        {
+            var result = await MediaPicker.CapturePhotoAsync();
+
+            if (result != null)
+            {
+                var stream = await result.OpenReadAsync();
+
+                resultImage.Source = ImageSource.FromStream(() => stream);
+            }
+        }
+    }
+}
